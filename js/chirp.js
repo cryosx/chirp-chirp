1855a02016a7886a596f76a229df3cb2df3b2322
cryosx <cyruswu.email@gmail.com>
2018-04-06 12:26:02 -1000

    Finished

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,15 @@
-function chirp(n){
+function chirp(n) {
   // FIX ME
+  function chirpHelper(num, result) {
+    if (num === 0) {
+      return result;
+    }
+    result = result + 'chirp ';
+    return chirpHelper(num - 1, result);
+  }
+  return chirpHelper(n, '');
 }
 
-$(document).ready(function(){
-  $("#result").html(chirp(3));
+$(document).ready(function() {
+  $('#result').html(chirp(3));
 });
